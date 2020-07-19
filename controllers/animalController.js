ac212c565eac3c881007ae35579de2b60b8e2805
SimJungUk <simju9397@gmail.com>
2020-07-19 23:21:45 +0900

    mission clear fix

--- controllers/animalController.js
+++ controllers/animalController.js
@@ -42,63 +42,75 @@ module.exports = {
             let { missionIdx } = req.body;
             console.log(animalIdx)
             let islandIdx = (await models.islands_animals.findOne({
-                where : {fk_animalIdx : animalIdx}
+                where: { fk_animalIdx: animalIdx }
             })).dataValues.fk_islandIdx;
             if (!animalIdx || !missionIdx) {
                 res.status(statCode.BAD_REQUEST).send(resUtil.successFalse(statCode.BAD_REQUEST, resMsg.NULL_VALUE));
                 throw "NULL VALUE"
             }
-            let updateAnimalProgress = await models.animals.update({
-                animalStatus: Sequelize.literal('animalStatus + 1'),
-                animalProgress: Sequelize.literal('animalProgress + 34'),
-                lastMissionClear: moment().format('YYYY-MM-DD HH:mm:ss'),
 
-            }, { where: { animalIdx } })
-            
-            let updateMissionStatus = await models.missions.update({
-                isCleared: 1
-            }, { where: { missionIdx } })
+
+            let missionInfo = await models.missions.findOne({
+                attributes: ["isCleared"],
+                where: { missionIdx }
+            })
             let animalInfo = await models.animals.findOne({
-                attributes: ["animalName","animalStatus", "animalProgress"],
-                where : {animalIdx}
+                attributes: ["animalName", "animalStatus", "animalProgress"],
+                where: { animalIdx }
             })
             let islandInfo = await models.islands.findOne({
-                attributes : ["islandName","islandProgress", "isLastProgressEven"],
-                where : {islandIdx}
+                attributes: ["islandName", "islandProgress", "isLastProgressEven", "islandStatus"],
+                where: { islandIdx }
             })
-            let {islandProgress, isLastProgressEven} = islandInfo.dataValues;
-            let {animalStatus} = animalInfo.dataValues;
-            if(animalStatus <= 3) {
-                if(isLastProgressEven == 0) {
+            let { islandProgress, isLastProgressEven, islandStatus } = islandInfo.dataValues;
+            let { animalStatus } = animalInfo.dataValues;
+            let { isCleared } = missionInfo.dataValues;
+            if (animalStatus == 3 && isCleared == 0) {
+                await models.animals.update({
+                    lastMissionClear: moment().format('YYYY-MM-DD HH:mm:ss'),
+                }, { where: { animalIdx } })
+            }
+
+            if (animalStatus < 3 && islandStatus < 2 && isCleared == 0) {
+                await models.animals.update({
+                    animalStatus: Sequelize.literal('animalStatus + 1'),
+                    animalProgress: Sequelize.literal('animalProgress + 34'),
+                    lastMissionClear: moment().format('YYYY-MM-DD HH:mm:ss'),
+                }, { where: { animalIdx } })
+
+                await models.missions.update({
+                    isCleared: 1
+                }, { where: { missionIdx } })
+                if (isLastProgressEven == 0) {
                     await models.islands.update({
                         islandProgress: Sequelize.literal('islandProgress + 6'),
-                        isLastProgressEven : 1
-                    }, {where : {islandIdx}})
+                        isLastProgressEven: 1
+                    }, { where: { islandIdx } })
                 }
                 else {
                     await models.islands.update({
                         islandProgress: Sequelize.literal('islandProgress + 5'),
-                        isLastProgressEven : 0
-                    }, {where : {islandIdx}})
+                        isLastProgressEven: 0
+                    }, { where: { islandIdx } })
                 }
-                if(islandProgress == 33.6) {
+                if (islandProgress == 28) {
                     await models.islands.update({
                         islandStatus: Sequelize.literal('islandStatus + 1')
-                    }, {where : {islandIdx}})
+                    }, { where: { islandIdx } })
                 }
-                if(islandProgress == 67.2) {
+                if (islandProgress == 61) {
                     await models.islands.update({
                         islandStatus: Sequelize.literal('islandStatus + 1')
-                    }, {where : {islandIdx}})
+                    }, { where: { islandIdx } })
                 }
             }
             let afterAnimalInfo = await models.animals.findOne({
-                attributes: ["animalName","animalStatus", "animalProgress"],
-                where : {animalIdx}
+                attributes: ["animalName", "animalStatus", "animalProgress"],
+                where: { animalIdx }
             })
             let afterIslandInfo = await models.islands.findOne({
-                attributes : ["islandName","islandProgress"],
-                where : {islandIdx}
+                attributes: ["islandName", "islandProgress", "islandStatus"],
+                where: { islandIdx }
             })
             res.status(statCode.OK).send(resUtil.successTrue(statCode.OK, resMsg.CLEAR_MISSION_SUCCESS, {
                 clearedMissionIdx: missionIdx,
